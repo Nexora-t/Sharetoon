c1475a33941c0bb130342d41b51293ebd19bb93d
Nexora-t <manyegolde1@gmail.com>
2026-08-24 18:31:55 +0100

    Create main.js

---
+++ file-transfer-app/electron/main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 500,
+        height: 700,
+        resizable: true,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            preload: path.join(__dirname, 'preload.js')
+        },
+        icon: path.join(__dirname, 'icon.ico')
+    });
+
+    mainWindow.loadFile(path.join(__dirname, '../src/index.html'));
+    
+    // قائمة بسيطة
+    Menu.setApplicationMenu(null);
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
